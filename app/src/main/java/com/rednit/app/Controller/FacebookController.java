7e6f5b3b6f00bf56624f7be76f8479cb96808a53
Yuri Campos <yuricampos@gmail.com>
2015-12-12 20:22:21 -0200

    Limpando app

--- app/src/main/java/com/rednit/app/Controller/FacebookController.java
+++ app/src/main/java/com/rednit/app/Controller/FacebookController.java
@@ -16,9 +16,13 @@ import java.util.ArrayList;
 public class FacebookController {
     private String likedPages;
 
-    String urlPostLikes = "http://54.207.112.184:3000/api/likes/";
-    String urlGetAccount = "http://54.207.112.184:3000/api/accounts/";
-    String urlPostAccount = "http://54.207.112.184:3000/api/accounts/";
+    //String urlPostLikes = "http://54.207.112.184:3000/api/likes/";
+    //String urlGetAccount = "http://54.207.112.184:3000/api/accounts/";
+    //String urlPostAccount = "http://54.207.112.184:3000/api/accounts/";
+
+    String urlPostLikes = "http://10.0.1.61:3000/api/likes/";
+    String urlGetAccount = "http://10.0.1.61:3000/api/accounts/";
+    String urlPostAccount = "http://10.0.1.61:3000/api/accounts/";
 
 
     //String urlPostLikes = "http://54.88.31.160:3000/api/likes/";
